3d40cf33d2ff62611ff958c68be0a581d2eb316b
Escobarkarla1708 <escobarkarla1708@gmail.com>
2026-05-16 17:42:05 -0300

    Agregados docstrings, typehints y comentarios faltantes.

--- tp2_AmoresanoEscobar.py
+++ tp2_AmoresanoEscobar.py
@@ -8,12 +8,12 @@ def pixel_art(imagen: Image.Image, tam_bloque: int, niveles_color: int) -> Image
     predominante de dicho bloque.
 
     Argumentos:
-        imagen (Image.Image): Objeto de imagen de la librería PIL (Pillow).
-        tam_bloque (int): Tamaño en píxeles del lado de cada bloque cuadrado.
-        niveles_color (int): Cantidad de tonos permitidos por cada canal (R, G, B).
+    imagen (Image.Image): Objeto de imagen de la librería PIL (Pillow).
+    tam_bloque (int): define que tan pixeleada se verá la imagen, el tamaño de cada bloque de píxeles.
+    niveles_color (int): Controla la reducción de color de la paleta. Numero menor imprime imagen con menor variedad de colores.
 
     Return:
-        Image.Image: Una nueva imagen con el efecto de Pixel Art aplicado.
+    Image.Image: Una nueva imagen con el efecto de Pixel Art aplicado.
     """
     # Convertimos la imagen a una cuadricula de píxeles RGB para procesarla (filas y columnas de color)
     cuadricula = np.array(imagen.convert('RGB'))
@@ -46,17 +46,16 @@ def pixel_art(imagen: Image.Image, tam_bloque: int, niveles_color: int) -> Image
 
 def ascii_art(imagen: Image.Image, ancho_ascii: int = 100) -> str:
     """
-    Transforma una imagen al estilo ASCII Art (en una cadena de caracteres ASCII).
-
-    #Ajusta el tamaño de la imagen, la convierte a escala de grises y mapea 
+    Transforma una imagen al estilo ASCII Art (en una cadena de caracteres ASCII),
+    ajusta el tamaño de la imagen, la convierte a escala de grises y convierte 
     la intensidad de cada píxel a un carácter según su densidad visual.
 
     Argumentos:
-        imagen (Image.Image): Objeto de imagen de la librería PIL (Pillow).
-        ancho_ascii (int): Ancho en caracteres del resultado ASCII.
+    imagen (Image.Image): Objeto de imagen de la librería PIL (Pillow).
+    ancho_ascii (int): Ancho en caracteres del resultado ASCII.
 
     Return:
-        str: Cadena de texto con saltos de línea que forma la imagen en ASCII.
+    str: Cadena de texto con saltos de línea que forma la imagen en ASCII.
     """
     # Paleta de caracteres ordenados de mayor a menor densidad visual (más oscuro a más claro)
     caracteres_ascii = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
@@ -65,14 +64,14 @@ def ascii_art(imagen: Image.Image, ancho_ascii: int = 100) -> str:
     imagen_gris = imagen.convert('L')
     ancho_original, alto_original = imagen_gris.size
 
-    # Ajustamos la proporcion de la imagen para que no se vea distorsionada al convertir a ASCII, ya que los caracteres no son cuadrados
+    # Ajustamos la proporcion de la imagen para que no se vea distorsionada al convertir a ASCII, ya que los caracteres son mas altos que anchos.
     proporcion = alto_original / ancho_original
     alto_ascii = int(ancho_ascii * proporcion * 0.5) # El numero 0.5 corrige la proporción de caracteres (porque sino la imagen quedaría muy alargada)
     
     # Redimensionamos la imagen para que se ajuste al ancho deseado
     imagen_redimensionada = imagen_gris.resize((ancho_ascii, alto_ascii))
         
-    # Mapeamos 0-255 a los caracteres ASCII, dividiendo el rango de intensidad por la cantidad de caracteres disponibles
+    # Asociamos cada valor entre 0 y 255 con un carácter ASCII dividiendo el rango por la cantidad de caracteres disponibles.
     div = 256 / len(caracteres_ascii)
 
     # Convertimos la imagen redimensionada a una cuadricula de píxeles
@@ -86,15 +85,13 @@ def ascii_art(imagen: Image.Image, ancho_ascii: int = 100) -> str:
     
     # Unimos todas las líneas con saltos de línea para formar la imagen ASCII completa.
     imagen_ascii = "\n".join(lineas_ascii)
+
     return imagen_ascii
 
 def pedir_metodo()-> str:
     """
-    Solicita al usuario elegir entre el efecto Pixel Art o ASCII Art.
-    
-    Utiliza un bucle infinito para garantizar que el usuario solo pueda avanzar 
-    si ingresa una de las dos opciones válidas, evitando errores en las 
-    etapas posteriores del procesamiento.
+    Solicita al usuario ingresar el efecto Pixel Art o ASCII Art.
+    Usa un ciclo para validar que la entrada sea correcta, aceptando solo 'pixel' o 'ascii'.
 
     Returns:
         str: El método seleccionado convertido a minúsculas ('pixel' o 'ascii').
@@ -105,23 +102,21 @@ def pedir_metodo()-> str:
             print("Método no reconocido. Por favor, seleccione 'pixel' o 'ascii'.")
         else:
             break
+
     return metodo
 
 def pedir_numero(mensaje: str, tipo: int, valor_default: int) -> int:
     """
-    Solicita un número al usuario y valida que sea un entero positivo.
+    Solicita un número al usuario y valida que sea un entero positivo,
+    sino, devuelve un valor predeterminado.
     
-    Si el usuario deja la entrada vacía o ingresa un valor no válido 
-    (letras, símbolos o números negativos), la función devuelve un 
-    valor predeterminado para asegurar que el programa continúe.
-
     Argumentos:
-        mensaje (str): El texto que se mostrará al usuario solicitando el dato.
-        tipo (int): 1 para tamaño de bloque y ancho ASCII, 2 para niveles de color, 3 para ancho ASCII; para verificar los valores minimos y maximos de cada variable.
-        valor_default (int): El número que se usará si la entrada es inválida.
+    mensaje (str): El texto que se mostrará al usuario solicitando el dato.
+    tipo (int): 1 para tamaño de bloque y ancho ASCII, 2 para niveles de color, 3 para ancho ASCII; para verificar los valores minimos y maximos de cada variable.
+    valor_default (int): El número que se usará si la entrada es inválida.
 
     Returns:
-        int: El número ingresado por el usuario o, en su defecto, el valor predeterminado.
+    int: El número ingresado por el usuario o el valor predeterminado.
     """    
     
     # Se recibe la entrada del usuario y se valida
@@ -147,41 +142,39 @@ def pedir_numero(mensaje: str, tipo: int, valor_default: int) -> int:
 
 def ruta_salida_valida(extension: str) -> str:
     """
-    Solicita al usuario una ruta de guardado y valida que sea factible.
-    
+    Solicita al usuario una ruta de guardado.    
     Asegura que el nombre no esté vacío, que la extensión sea correcta y que 
     la carpeta de destino exista. Si el archivo ya existe, añade un número 
     automáticamente para evitar sobrescribir datos.
 
     Argumentos:
-        extension_requerida (str): La extensión que debe tener el archivo (ej: '.png' o '.txt').
+    extension_requerida (str): La extensión que debe tener el archivo.
 
     Returns:
-        str: Una ruta de archivo válida y verificada.
+    str: Una ruta de archivo válida y verificada.
     """
 
     # El bucle se mientras la ruta ingresada no sea válida
     while True:
         # Se solicita la ruta de salida al usuario
-        ruta = input(f"Seleccione el nombre o ruta de salida ({extension}): ").strip()
+        ruta = input(f"Seleccione la ruta para guardar la imagen procesada ({extension}): ").strip()
         
         # Validamos que no esté vacío
         if not ruta:
             print("Error: El nombre del archivo no puede estar vacío.")
-            continue  # Si esta vacio, vuelve a preguntar por la ruta hasta que no lo esté
+            continue
         
-        # Aseguramos que la ruta tenga la extensión correcta, si no la tiene, se la añadimos
+        # Aseguramos que la ruta tenga la extensión correcta, y si no, se anade automáticamente
         if not ruta.lower().endswith(extension):
             ruta += extension
             
         # Validamos que la carpeta exista
         directorio = os.path.dirname(ruta)
-        # Si el directorio no es vacío y no existe, mostramos un error y volvemos a preguntar por la ruta
         if directorio != "" and not os.path.isdir(directorio):
             print(f"Error: La carpeta '{directorio}' no existe. Intente otra ruta.")
             continue
         
-        # Verificamos si la ruta ingresada ya existe.
+        # Verificamos si la ruta ingresada ya existe para evitar sobrescribirlo.
         if os.path.exists(ruta):
             # Separamos nombre de extensión para poder modificar el nombre sin cambiar la extensión
             nombre, ext = os.path.splitext(ruta)
@@ -195,35 +188,70 @@ def ruta_salida_valida(extension: str) -> str:
         return ruta
 
 def guardar_ascii_art(ascii_art: str, ruta_salida: str): 
+    """
+    Guarda la cadena de texto del arte ASCII en un archivo de texto (.txt).
+
+    Argumentos:
+    ascii_art (str): La cadena de texto del arte ASCII.
+    ruta_salida (str): La ruta de destino donde se creará el archivo.
+    """
     with open(ruta_salida, 'w') as f:
         f.write(ascii_art)
 
-#agregar docstrings y comentarios a metodos 
-#y corregir algunos docstrings
-# cambios para realizar commits:
 def metodo_pixel(imagen: Image.Image):
+    """
+    Controla el proceso para transformar y guardar la imagen en modo Pixel Art.
+    Se encarga de pedir al usuario el tamaño del bloque y los niveles de color, 
+    aplicar el efecto sobre la cuadrícula de la imagen y gestionar el guardado 
+    del archivo final en formato PNG.
+
+    Argumentos:
+    imagen (Image.Image): imagen ya cargada.
+    """
+    # Se solicita al usuario los parametros necesarios para el efecto Pixel Art
     tam_bloque = pedir_numero("Ingrese el tamaño del bloque", 1, 10)
     niveles_color = pedir_numero("Ingrese los niveles de color", 2, 4)
 
+    # Procesamos la imagen con los parametros obtenidos
     resultado = pixel_art(imagen, tam_bloque, niveles_color)
 
+    # Guardamos la imagen editada. Se asegura que la ruta sea válida y que el archivo se guarde con extensión .png
     ruta_salida = ruta_salida_valida(".png")
     if ruta_salida:
         resultado.save(ruta_salida)
         print(f"Imagen guardada en: {ruta_salida}")
 
 def metodo_ascii(imagen: Image.Image):
+    """
+    Controla el proceso para transformar y guardar la imagen en modo ASCII Art.
+    Se encarga de pedir al usuario el ancho en caracteres del resultado ASCII,
+    aplicar el efecto sobre la cuadrícula de la imagen y gestionar el guardado
+    del archivo final en formato de texto (.txt).
+    Argumentos:
+    imagen (Image.Image): imagen ya cargada.
+    """
+    
+    # Se solicita al usuario el ancho en caracteres del resultado ASCII
     ancho_ascii = pedir_numero("Ingrese el ancho ASCII [1-1000]", 3, 100)
 
+    # Procesamos la imagen con el parametro obtenido
     resultado = ascii_art(imagen, ancho_ascii)
     
+    # Guardamos el arte ASCII generado. Se asegura que la ruta sea válida y que el archivo se guarde con extensión .txt
     ruta_salida = ruta_salida_valida(".txt")
     if ruta_salida:
         guardar_ascii_art(resultado, ruta_salida)
         print(f"Archivo guardado en: {ruta_salida}")
 
 def main():
+    """
+    Punto de entrada principal del programa.
+    Solicita al usuario el archivo de la imagen a procesar, 
+    ejecuta el metodo de transformación seleccionado y 
+    maneja cualquier error inesperado durante la ejecución del programa.
+    """
     try:
+        # Solicitud de la ruta de la imagen al usuario y validación de su existencia
         ruta = input("Ingrese la ruta de la imagen: ").strip()
         if not os.path.exists(ruta):
             print("No se encontró la imagen. Por favor, verifique la ruta e intente nuevamente.")
@@ -235,6 +263,7 @@ def main():
             print(f"Error al abrir la imagen: {e}")
             return
         
+        # Selección del modo de transformación
         metodo = pedir_metodo()
 
         if metodo == "pixel":
@@ -243,6 +272,7 @@ def main():
         elif metodo == "ascii":
             metodo_ascii(imagen)
 
+    # Captura de cualquier error inesperado durante la ejecución del programa
     except Exception as e:
         print(f"Ocurrió un error inesperado: {e}")
 
